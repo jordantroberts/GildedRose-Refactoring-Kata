aba72336790365388b2b20e40205f2cd8c80f191
Jordan Roberts <jordanroberts@talk21.com>
2019-06-06 11:15:01 +0100

    Quality of item never negative and once sell by date passed quality degrades quick

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -15,10 +15,22 @@ describe GildedRose do
       expect(items[0].quality).to eq 9
     end
 
+    it 'quality of item is never negative' do
+      items = [Item.new("foo", 10, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 0
+    end
+
     it 'aged brie increases in quality' do
       items =[Item.new("Aged Brie", 10, 5)]
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 6
+    end
+
+    it 'once sell by date passed, quality degrades quicker' do
+      items =[Item.new("toast", 0, 4)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 2
     end
   end
 end
